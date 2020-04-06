fa0bf63fa664477ab9b0e7c4423397c5e80dcc03
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2020-04-06 14:46:25 +0700

    Declare config type

--- lib/ex_authn/config.ex
+++ lib/ex_authn/config.ex
@@ -5,11 +5,29 @@ defmodule ExAuthn.Config do
 
   alias ExAuthn.Protocol
 
+  @type t :: %__MODULE__{
+          id: String.t(),
+          display_name: String.t(),
+          icon: String.t(),
+          origin: String.t(),
+          attestation_preference: Protocol.conveyance_preference(),
+          authenticator_selection: Protocol.authenticator_selection(),
+          timeout: pos_integer()
+        }
+
   @type relying_party :: %{
           id: String.t(),
           display_name: String.t(),
           icon: String.t()
         }
+
+  defstruct id: nil,
+            display_name: nil,
+            icon: nil,
+            origin: nil,
+            attestation_preference: nil,
+            authenticator_selection: nil,
+            timeout: nil
 
   @spec relying_party() :: relying_party()
   def relying_party do
@@ -20,6 +38,11 @@ defmodule ExAuthn.Config do
     }
   end
 
+  @spec origin() :: String.t()
+  def origin do
+    "http://localhost:4000"
+  end
+
   @spec timeout :: pos_integer()
   def timeout do
     60000
@@ -28,5 +51,10 @@ defmodule ExAuthn.Config do
   @spec attestation_preference() :: Protocol.conveyance_preference()
   def attestation_preference do
     :direct
+  end
+
+  @spec user_verification() :: Protocol.user_verification_requirement()
+  def user_verification do
+    :required
   end
 end

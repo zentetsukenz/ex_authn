fdc08e2f401909d523db983cd15eb382234638d7
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2021-01-28 14:11:24 +0700

    WebAuthn Challenge

--- lib/ex_authn/web_authn/challenge.ex
+++ lib/ex_authn/web_authn/challenge.ex
@@ -1,19 +1,30 @@
 defmodule ExAuthn.WebAuthn.Challenge do
+  @moduledoc """
+  A string intended to be used for generating the newly created credential's
+  attestation object.
+  """
+  @moduledoc since: "1.0.0"
+
   @type t :: String.t()
 
+  @minimum_size 16
+
   @doc """
-  Generate challenge to be sent to authenticator.
+  Generate challenge to be sent to authenticator as a part of attestation
+  object.
 
   ## Examples
 
       iex> ExAuthn.WebAuthn.Challenge.generate(15)
-      {:error, "length must be at least 16"}
-  """
-  @spec generate(pos_integer()) :: {:ok, t()} | {:error, String.t()}
-  def generate(length_in_byte \\ 16)
+      {:error, "length must be greater than or equal to 16"}
 
-  def generate(length_in_byte) when length_in_byte < 16 do
-    {:error, "length must be at least 16"}
+  """
+  @doc since: "1.0.0"
+  @spec generate(pos_integer()) :: {:ok, t()} | {:error, String.t()}
+  def generate(length_in_byte \\ @minimum_size)
+
+  def generate(length_in_byte) when length_in_byte < @minimum_size do
+    {:error, "length must be greater than or equal to #{@minimum_size}"}
   end
 
   def generate(length_in_byte) do
--- test/ex_authn/web_authn/challenge_test.exs
+++ test/ex_authn/web_authn/challenge_test.exs
@@ -1,4 +1,13 @@
 defmodule ExAuthn.WebAuthn.ChallengeTest do
   use ExUnit.Case, async: true
   doctest ExAuthn.WebAuthn.Challenge
+
+  alias ExAuthn.WebAuthn.Challenge
+
+  describe "generate/1" do
+    test "returns ok with random generated base64 challenge from specified challenge size" do
+      assert {:ok, challenge} = Challenge.generate(32)
+      assert 32 = challenge |> Base.url_decode64!() |> byte_size()
+    end
+  end
 end

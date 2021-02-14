b3ae9ff8c43cc4071e5acd862b9a9d610516dcaa
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2021-02-14 20:29:28 +0700

    Drop 1.9.4 support
    In favored of put compiler options.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExAuthn.MixProject do
     [
       app: :ex_authn,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
